02efbe1e153c61366603c25bb880c06e964d3339
anudeepkatukojwala <anudeep.katukojwala@gmail.com>
2023-04-25 10:36:48 -0700

    Change to DFS code to correct a mistake of not having parenthesis

--- PreGraphDrawingOperations.java
+++ PreGraphDrawingOperations.java
@@ -1,24 +1,25 @@
 import java.util.*;
 import org.apache.commons.math4.legacy.linear.*;
 
+
 public class PreGraphDrawingOperations {
-    List<Integer> xValues;
-    List<Integer> yValues;
+    List<Double> xValues;
+    List<Double> yValues;
     List<String> edges;
 
-    public PreGraphDrawingOperations(List<Integer> xValues, List<Integer> yValues, List<String> edges){
+    public PreGraphDrawingOperations(List<Double> xValues, List<Double> yValues, List<String> edges){
         this.xValues = xValues;
         this.yValues = yValues;
         this.edges = edges;
     }
 
-    public boolean checkIfAnyThreePointsAreCollinear(List<HashMap<Integer, Integer>> vertexAngleMapping, List<List<Integer>> rotationSystem){
+    public boolean checkIfAnyThreePointsAreCollinear(List<HashMap<Integer, Double>> vertexAngleMapping, List<List<Integer>> rotationSystem){
         for(int i=0;i<rotationSystem.size();i++){
             List<Integer> currList = rotationSystem.get(i);
-            HashMap<Integer, Integer> currMap = vertexAngleMapping.get(i);
+            HashMap<Integer, Double> currMap = vertexAngleMapping.get(i);
             for(int j=0;j<currList.size()-1;j++){
-                int firstAngle = vertexAngleMapping.get(i).get(currList.get(j));
-                int secondAngle = vertexAngleMapping.get(i).get(currList.get(j+1));
+                double firstAngle = vertexAngleMapping.get(i).get(currList.get(j));
+                double secondAngle = vertexAngleMapping.get(i).get(currList.get(j+1));
                 double diffAngle = secondAngle-firstAngle;
                 if(diffAngle<=Math.pow(10, -3)){
                     return false;
@@ -53,27 +54,28 @@ public class PreGraphDrawingOperations {
     }
 
     public boolean findIfEdgesCross(int firstEdgeFirstVertex, int firstEdgeSecondVertex, int secondEdgeFirstVertex, int secondEdgeSecondVertex){
-        int ax = xValues.get(firstEdgeFirstVertex);
-        int ay = yValues.get(firstEdgeFirstVertex);
-        int bx = xValues.get(firstEdgeSecondVertex);
-        int by = yValues.get(firstEdgeSecondVertex);
-        int cx = xValues.get(secondEdgeFirstVertex);
-        int cy = yValues.get(secondEdgeFirstVertex);
-        int dx = xValues.get(secondEdgeSecondVertex);
-        int dy = yValues.get(secondEdgeSecondVertex);
+        double ax = xValues.get(firstEdgeFirstVertex);
+        double ay = yValues.get(firstEdgeFirstVertex);
+        double bx = xValues.get(firstEdgeSecondVertex);
+        double by = yValues.get(firstEdgeSecondVertex);
+        double cx = xValues.get(secondEdgeFirstVertex);
+        double cy = yValues.get(secondEdgeFirstVertex);
+        double dx = xValues.get(secondEdgeSecondVertex);
+        double dy = yValues.get(secondEdgeSecondVertex);
 
         //Using Commons Math Library
         RealMatrix coefficients = new Array2DRowRealMatrix(new double[][] { {(bx-ax), (-dx+cx)}, {(by-ay), (-dy+cy)} }, false);
         DecompositionSolver solver = new org.apache.commons.math4.legacy.linear.LUDecomposition(coefficients).getSolver();
 
         RealVector constants = new ArrayRealVector(new double[] {(cx-ax), (cy-ay)}, false);
+
         RealVector solution = solver.solve(constants);
         System.out.println("\nCommons Math library solution");
         double t1 = solution.getEntry(0);
         double t2 = solution.getEntry(1);
         System.out.println(solution.getEntry(0)+" "+solution.getEntry(1));
         if((t1>=0 && t1<=1) && (t2>=0 && t2<=1)){
-            return false;
+            return true;
         }
         else{
             return false;
